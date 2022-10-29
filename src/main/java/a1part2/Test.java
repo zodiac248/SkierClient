a43008f2247d6cbf8c97ffc6fbc35e756c1133f0
zodiac248 <roy971128@gmail.com>
2022-10-28 22:58:16 -0700

    update rabbitmq

--- src/main/java/a1part2/Test.java
+++ src/main/java/a1part2/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        ThreadManager threadManager = new ThreadManager(200000,1000,32);
+        ThreadManager threadManager = new ThreadManager(200000,5000,256);
         int successRequests = threadManager.sendRequest();
         //print message is in threadManager class
 
